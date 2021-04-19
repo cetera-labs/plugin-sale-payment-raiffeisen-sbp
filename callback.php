1bef8b46dbb9985e1c4a8f096f35cb4e51484e11
Roman Romanov <nicodim@mail.ru>
2021-04-19 15:46:09 +0300

    dev

--- callback.php
+++ callback.php
@@ -12,6 +12,24 @@ try {
 
     $headers = getallheaders();
     
+    /*
+    $requestBody = [
+        'transactionId' => '156781',
+        'qrId' => 'AD100022A20MJ2TV9N79UESV97I091TO',
+        'sbpMerchantId' => 'MA0000091561',
+        'merchantId' => '1786926001',
+        'amount' => '1',
+        'currency' => 'RUB',
+        'transactionDate' => '2021-04-19T15:33:48+03:00',
+        'paymentStatus' => 'SUCCESS',
+        'additionalInfo' => '',
+        'order' => '59528',
+        'createDate' => '2021-04-19T15:31:59+03:00', 
+    ];
+    $headers = [
+        'X-Api-Signature-Sha256' => '491a68d56081262d083b1314abfc3e94de9ed361048b123aea75a7c57abe0fcb'
+    ];
+    */
     print_r($requestBody);
     print_r($headers);
 
@@ -19,6 +37,7 @@ try {
 	$gateway = $order->getPaymentGateway();
     
     $oid = $gateway->getOrderByTransaction( $requestBody['qrId'] );
+        
     if ($oid != $order->id) {
         throw new \Exception('Order check failed');
     }
@@ -31,7 +50,7 @@ try {
         $requestBody['transactionDate'],
     ]), $gateway->params['secretKey']);
     
-    if ($hash != $headers['X-Api-Signature-SHA256']) {
+    if ($hash != $headers['X-Api-Signature-Sha256']) {
         throw new \Exception('X-Api-Signature check failed');
     }
 		
